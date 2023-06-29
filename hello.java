26e80275d47d62ed874dc7e1ab7a301ef20cca1c
MrBear0701 <86502537@qq.com>
2023-06-29 16:14:53 +0800

    hello.java

---
+++ hello.java
@@ -0,0 +1,42 @@
+package mrbear;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
